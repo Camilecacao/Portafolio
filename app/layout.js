89049ce9e83d78aeb15782cbbdd9cf7f93ae97b0
Camilita <camiej.nutri@gmail.com>
2024-02-11 11:56:07 -0300

    RRSS

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider";
 import Appnavbar from "@/components/Appnavbar";
+import { Socials } from "@/components/Socials";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Provider>
           <Appnavbar/>
+          <Socials/>
            {children}
         </Provider>
         </body>
